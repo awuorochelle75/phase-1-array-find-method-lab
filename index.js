fd965417489e073043e9ad4ce2e4392a537f33a0
Rochelle Awuor <awuorochelle75@gmail.com>
2025-03-14 12:12:06 +0300

    modifies index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function superbowlWin(record){
+    let records= [
+        {year:'2015',result:"W"},
+        {year:'2014', result:"N/A"},
+        {year:'2013',result:'L'},
+]
+    let win = record.find(item => item.result === 'W');
+
+    return win ? win.year : undefined; 
+          
+}superbowlWin()
+     
